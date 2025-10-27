f486269048acd79017086399acf9043e87a1cfbe
Jean-Christophe Dauchy <jean-christophe.dauchy@moodys.com>
2025-10-27 10:39:50 +0100

    setup build - 2

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,6 @@
 go install github.com/magefile/mage
 # Load project version
 PKG_VERSION=$(cat ./package.json | jq -r ".version")
-# First, clean the project and previous releases
-echo "Removing old files..."
-rm -rf ./dist/ ./node_modules/ ./*.tar.gz
 # Compile it again
 echo "Compiling code..."
 npm ci && npm run build && mage -v
